c6f668f9411cdc4557c5fb794a1e4d626bab52c2
Faisal Ramdan <faisalramdan.id@gmail.com>
2023-06-17 08:07:37 +0700

    Fix overlay back button ini debug widget

--- lib/debug.dart
+++ lib/debug.dart
@@ -15,31 +15,31 @@ Widget debugView({
         Positioned(
           right: -8,
           bottom: 100,
-          child: Container(
-            width: 30.0,
-            padding: const EdgeInsets.all(6.0),
-            decoration: const BoxDecoration(
-              color: Colors.black,
-              borderRadius: BorderRadius.all(
-                Radius.circular(
-                  16.0,
+          child: InkWell(
+            onTap: () => Get.back(),
+            child: Container(
+              width: 30.0,
+              padding: const EdgeInsets.all(6.0),
+              decoration: const BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(
+                    16.0,
+                  ),
                 ),
               ),
-            ),
-            child: Column(
-              children: [
-                InkWell(
-                  onTap: () => Get.back(),
-                  child: const Icon(
+              child: Column(
+                children: [
+                  const Icon(
                     Icons.arrow_back,
                     color: Colors.white,
                     size: 12.0,
                   ),
-                ),
-                const SizedBox(
-                  height: 12.0,
-                ),
-              ],
+                  const SizedBox(
+                    height: 12.0,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
