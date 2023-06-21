10e6945f96ea385a34980ee42b6f4cb2b29ac47a
FebriAnnisaDev <febriannisarisa21@gmail.com>
2023-06-21 11:44:15 +0700

    Account Settings update

---
+++ lib/module/account_setting1/data/data_dummy.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class AccountData {
+  static List accountDataList = [
+    {
+      "leading_icon": Icons.person,
+      "title": "Profile Information",
+      "subtitle": "Change your account information",
+      "action_icon": Icons.chevron_right,
+    },
+    {
+      "leading_icon": Icons.lock,
+      "title": "Change Password",
+      "subtitle": "Change your password",
+      "action_icon": Icons.chevron_right,
+    },
+    {
+      "leading_icon": Icons.wallet,
+      "title": "Payment Methods",
+      "subtitle": "Add your credit & debit cards",
+      "action_icon": Icons.chevron_right,
+    },
+    {
+      "leading_icon": Icons.location_on_rounded,
+      "title": "Locations",
+      "subtitle": "Add or remove your delivery locations",
+      "action_icon": Icons.chevron_right,
+    },
+    {
+      "leading_icon": Icons.public,
+      "title": "Add Social Account",
+      "subtitle": "Add Facebook, Twitter etc",
+      "action_icon": Icons.chevron_right,
+    },
+    {
+      "leading_icon": Icons.share,
+      "title": "Refer to Friends",
+      "subtitle": "Get \$10 for referring friends",
+      "action_icon": Icons.chevron_right,
+    },
+    {
+      "leading_icon": Icons.notifications,
+      "title": "Push Notifications",
+      "subtitle": "For daily update you will get it",
+      "action_icon": null,
+    },
+    {
+      "leading_icon": Icons.notifications,
+      "title": "SMS Notifications",
+      "subtitle": "For daily update you will get it",
+      "action_icon": null,
+    },
+    {
+      "leading_icon": Icons.notifications,
+      "title": "Promotional Notifications",
+      "subtitle": "For daily update you will get it",
+      "action_icon": null,
+    },
+    {
+      "leading_icon": Icons.star,
+      "title": "Rate Us",
+      "subtitle": "Rate us on playstore, app store",
+      "action_icon": Icons.chevron_right,
+    },
+    {
+      "leading_icon": Icons.book,
+      "title": "FAQ",
+      "subtitle": "Frequently asked questions",
+      "action_icon": Icons.chevron_right,
+    },
+    {
+      "leading_icon": Icons.logout,
+      "title": "Logout",
+      "subtitle": null,
+      "action_icon": Icons.chevron_right,
+    },
+  ];
+}
--- lib/module/account_setting1/view/account_setting1_view.dart
+++ lib/module/account_setting1/view/account_setting1_view.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hyper_ui/core.dart';
-import '../controller/account_setting1_controller.dart';
+
+import '../data/data_dummy.dart';
 
 class AccountSetting1View extends StatefulWidget {
   const AccountSetting1View({Key? key}) : super(key: key);
@@ -17,7 +19,132 @@ class AccountSetting1View extends StatefulWidget {
         child: Container(
           padding: const EdgeInsets.all(10.0),
           child: Column(
-            children: const [],
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 24.0,
+              ),
+              Text(
+                "Account Settings",
+                style: TextStyle(
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                "Update your settings like notifications, payments, profile edit etc.",
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: secondaryTextColor,
+                ),
+              ),
+              const SizedBox(
+                height: 24.0,
+              ),
+              ListView.builder(
+                itemCount: AccountData.accountDataList.length,
+                shrinkWrap: true,
+                physics: const ScrollPhysics(),
+                itemBuilder: (BuildContext context, int index) {
+                  var item = AccountData.accountDataList[index];
+                  bool value = true;
+
+                  return Column(
+                    children: [
+                      Row(
+                        children: [
+                          Icon(
+                            item['leading_icon'],
+                            color: index == 9 ? primaryColor : Colors.grey[900],
+                          ),
+                          const SizedBox(
+                            width: 20.0,
+                          ),
+                          if (item['subtitle'] == null)
+                            Text(
+                              item['title'],
+                              style: TextStyle(
+                                fontSize: 16.0,
+                              ),
+                            ),
+                          if (item['subtitle'] != null)
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  item['title'],
+                                  style: TextStyle(
+                                    fontSize: 16.0,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 10.0,
+                                ),
+                                Text(
+                                  item['subtitle'],
+                                  style: TextStyle(
+                                    fontSize: 14.0,
+                                    color: secondaryTextColor,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          Spacer(),
+                          Icon(
+                            item['action_icon'],
+                          ),
+                          if (item['action_icon'] == null)
+                            CupertinoSwitch(
+                              value: value,
+                              onChanged: (value) {},
+                            ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20.0,
+                      ),
+                      if (index == 5)
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 20,
+                            bottom: 15,
+                          ),
+                          child: Row(
+                            children: [
+                              Text(
+                                "NOTIFICATIONS",
+                                style: TextStyle(
+                                  fontSize: 16.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      if (index == 8)
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 20,
+                            bottom: 15,
+                          ),
+                          child: Row(
+                            children: [
+                              Text(
+                                "MORE",
+                                style: TextStyle(
+                                  fontSize: 16.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                    ],
+                  );
+                },
+              ),
+            ],
           ),
         ),
       ),
