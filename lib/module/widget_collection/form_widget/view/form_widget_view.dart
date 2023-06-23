658d52723109e8401cb5e41d791520d7e173b2df
deny <deny@google.com>
2023-06-23 19:57:30 +0700

    .

--- lib/module/widget_collection/form_widget/view/form_widget_view.dart
+++ lib/module/widget_collection/form_widget/view/form_widget_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hyper_ui/core.dart';
+import 'package:hyper_ui/shared/widget/form/radio_with_header/radio_with_header.dart';
 
 class FormWidgetView extends StatefulWidget {
   const FormWidgetView({Key? key}) : super(key: key);
@@ -25,6 +26,46 @@ class FormWidgetView extends StatefulWidget {
                 child: const Text("HUIWidgetDemoView"),
               ),
               Divider(),
+              //#TEMPLATE q_radiofield_with_header
+              QRadioFieldWithHeader(
+                label: "Gender",
+                validator: Validator.atLeastOneitem,
+                items: [
+                  {
+                    "label": "Female",
+                    "value": 1,
+                  },
+                  {
+                    "label": "Male",
+                    "value": 2,
+                  }
+                ],
+                onChanged: (value, label) {},
+              ),
+              //#END
+              QRadioFieldWithHeader(
+                label: "Gender",
+                validator: Validator.atLeastOneitem,
+                items: [
+                  {
+                    "label": "XXX",
+                    "value": 1,
+                  },
+                  {
+                    "label": "YYY",
+                    "value": 2,
+                  },
+                  {
+                    "label": "ZZZ",
+                    "value": 2,
+                  },
+                  {
+                    "label": "DDD",
+                    "value": 2,
+                  }
+                ],
+                onChanged: (value, label) {},
+              ),
             ],
           ),
         ),
---
+++ lib/shared/widget/form/radio_with_header/radio_with_header.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+
+class QRadioFieldWithHeader extends StatefulWidget {
+  final String label;
+  final String? hint;
+  final String? helper;
+  final List<Map<String, dynamic>> items;
+  final String? Function(List<Map<String, dynamic>> item)? validator;
+  final Function(dynamic value, String? label) onChanged;
+  final String? value;
+
+  const QRadioFieldWithHeader({
+    Key? key,
+    required this.label,
+    required this.items,
+    this.validator,
+    this.hint,
+    this.helper,
+    this.value,
+    required this.onChanged,
+  }) : super(key: key);
+
+  @override
+  State<QRadioFieldWithHeader> createState() => _QRadioFieldWithHeaderState();
+}
+
+class _QRadioFieldWithHeaderState extends State<QRadioFieldWithHeader> {
+  List<Map<String, dynamic>> items = [];
+
+  @override
+  void initState() {
+    super.initState();
+    for (var item in widget.items) {
+      items.add(Map.from(item));
+      if (items.last["value"] == widget.value) {
+        items.last["checked"] = true;
+      }
+    }
+  }
+
+  setAllItemsToFalse() {
+    for (var item in items) {
+      item["checked"] = false;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(
+        bottom: 12.0,
+      ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Text(
+                  "${widget.label}",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Card(
+                color: Color(0xfffcebd2),
+                elevation: 0.0,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 8.0,
+                    horizontal: 16.0,
+                  ),
+                  child: Text(
+                    "Required",
+                    style: const TextStyle(
+                      fontSize: 14.0,
+                      color: Color(0xfff1a63d),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          FormField(
+            initialValue: false,
+            validator: (value) => widget.validator!(items),
+            enabled: true,
+            builder: (FormFieldState<bool> field) {
+              return InputDecorator(
+                decoration: InputDecoration(
+                  fillColor: Colors.transparent,
+                  border: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  errorBorder: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  disabledBorder: InputBorder.none,
+                  focusedErrorBorder: InputBorder.none,
+                ),
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: items.length,
+                  itemBuilder: (context, index) {
+                    var item = items[index];
+                    return Container(
+                      decoration: BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            width: 1.0,
+                            color: Color(0xfff0f0f0),
+                          ),
+                        ),
+                      ),
+                      child: RadioListTile(
+                        title: Text("${item["label"]}"),
+                        groupValue: true,
+                        value: item["checked"] ?? false,
+                        onChanged: (val) {
+                          setAllItemsToFalse();
+                          bool newValue = val ? false : true;
+                          items[index]["checked"] = newValue;
+                          field.didChange(true);
+                          setState(() {});
+
+                          String? label = items[index]["label"];
+                          int foundIndex = items
+                              .indexWhere((item) => item["label"] == label);
+                          dynamic value = items[foundIndex]["value"];
+                          widget.onChanged(value, label);
+                        },
+                      ),
+                    );
+                  },
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
